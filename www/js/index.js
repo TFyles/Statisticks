aa5a42f61e24fe46dd74d5a7101d3f9d8bd6a468
TFyles <fylt1_13@uni.worc.ac.uk>
2014-10-03 20:06:11 +0100

    isrusr
    ir6suiui

--- www/js/index.js
+++ www/js/index.js
@@ -46,9 +46,34 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+    document.addEventListener("deviceready", onDeviceReady, false);
     function onDeviceReady() {
         console.log(device.cordova);
-        document.write("You are using" + device.cordova);
+        document.write("You are using" + device.cordova)
     }
     
 };
+
+var app2 = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    function onDeviceReady() {
+        console.log(device.cordova);  
+        document.write("You are using" + device.cordova) ;
+    }
+    
+};
+
