85b0e0e1fac61eab909908920621f0bbebc2e7e6
TFyles <fylt1_13@uni.worc.ac.uk>
2014-10-17 11:01:23 +0100

    gsgweg
    ewgewg

--- www/js/index.js
+++ www/js/index.js
@@ -51,4 +51,14 @@ var app = {
     }
 };
 
+    window.localStorage.setItem("Phone", "BlackBerry");
+    window.localStorage.setItem("Wallet", "Brown");
+    window.localStorage.setItem("Keyboard", "Qwerty");
+    window.localStorage.setItem("Screen", "24");
+    window.localStorage.setItem("Shoes", "Brown");
+
+    var phone = window.localStorage.getItem(“Phone”);
+    document.write("Hi");
+    $("#area").text("Your phone is: " + phone);   
+    
 
