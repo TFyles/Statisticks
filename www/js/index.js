e790afc988d2a320e022493a8d3a5f9094572db6
TFyles <fylt1_13@uni.worc.ac.uk>
2014-11-07 10:44:51 +0000

    gtwetet
    wettet

--- www/js/index.js
+++ www/js/index.js
@@ -53,4 +53,25 @@ var app = {
     }
 };
 
+$(document).on("pagecreate","#pageone",function(){
+  
+    $('#tapholdtext').on("taphold",function(){
+        $(this).hide();
+    });                       
+
+    $('#taptext').on("tap",function(){
+        $(this).css('color', 'red');
+    }); 
+
+    $('#swipetext').on("swipeleft",function(){
+        $(this).css('color', 'green');
+    });  
+
+    $('#swipetext').on("swiperight",function(){
+        $(this).css('color', 'blue');
+    });  
+
+
+});
+
 
