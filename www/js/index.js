29ab52560ed2e66c9b889cd3c5e79f48ac95e532
TFyles <fylt1_13@uni.worc.ac.uk>
2014-11-28 11:01:46 +0000

    5y54y4
    5y45y

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
+var id;
  //when the jQuery Mobile page is initialised
 $(document).on('pageinit', function() {
     
