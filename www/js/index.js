a7d861aabf45a19908682b38b18adaea49bc0bfa
TFyles <fylt1_13@uni.worc.ac.uk>
2014-11-28 09:32:26 +0000

    y53y35y
    y34y34y3

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,53 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ 
+ //when the jQuery Mobile page is initialised
+$(document).on('pageinit', function() {
+    
+    //set up listener for button click
+    $(document).on('click', getPosition);
+    
+    //change time box to show message
+    $('#time').val("Press the button to get location data");
+    
+});
+
+
+//Call this function when you want to get the current position
+function getPosition() {
+    
+    //change time box to show updated message
+    $('#time').val("Getting data...");
+    
+    //instruct location service to get position with appropriate callbacks
+    navigator.geolocation.getCurrentPosition(successPosition, failPosition);
+}
+
+
+//called when the position is successfully determined
+function successPosition(position) {
+    
+    //You can find out more details about what the position obejct contains here:
+    // http://www.w3schools.com/html/html5_geolocation.asp
+    
+
+    //lets get some stuff out of the position object
+    var time = position.timestamp;
+    var latitude = position.coords.latitude;
+    
+    //OK. Now we want to update the display with the correct values
+    $('#time').val("Recieved data at " + time);
+    $('#lattext').val("I should contain the latitude data...");
+    
+}
+
+//called if the position is not obtained correctly
+function failPosition(error) {
+    //change time box to show updated message
+    $('#time').val("Error getting data: " + error);
+    
+}
 
  
 var app = {
