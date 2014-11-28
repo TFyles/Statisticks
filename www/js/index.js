22fdc0d373b989ad9427997c30cad7cce1bcc8ea
TFyles <fylt1_13@uni.worc.ac.uk>
2014-11-28 09:41:43 +0000

    56wi56i
    5u45u

--- www/js/index.js
+++ www/js/index.js
@@ -50,10 +50,12 @@ function successPosition(position) {
     //lets get some stuff out of the position object
     var time = position.timestamp;
     var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
     
     //OK. Now we want to update the display with the correct values
     $('#time').val("Recieved data at " + time);
     $('#lattext').val(latitude);
+    $('#longtext').val(longitude);
     
 }
 
